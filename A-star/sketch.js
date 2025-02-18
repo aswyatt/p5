dba55fb1a7d72643d53de2f90aad77ce2e6f7f14
Adam S Wyatt <a.wyatt@imperial.ac.uk>
2025-02-18 11:10:43 +0000

    More improvements

--- A-star/sketch.js
+++ A-star/sketch.js
@@ -1,13 +1,17 @@
-let grid, scaleX, scaleY, scaleP, count;
+let grid, scaleX, scaleY, scaleP, speed;
 let chebyshev = (a, b) =>
   Math.max(Math.abs(a.nx - b.nx), Math.abs(a.ny - b.ny));
 let distance = (a, b) => Math.sqrt((a.nx - b.nx) ** 2 + (a.ny - b.ny) ** 2);
 let manhatten = (a, b) => Math.abs(a.nx - b.nx) + Math.abs(a.ny - b.ny);
 
-
-let actualDistance = chebyshev;
+// let actualDistance = chebyshev;
+// let actualDistance = (a, b) =>1/(1/chebyshev(a,b) + 1/distance(a,b));
 let heuristicDistance = chebyshev;
+let actualDistance = distance;
+// let heuristicDistance = distance;
+speed = 10;
 
+let diagonals = false;
 function drawBoundary() {
   push();
   noFill();
@@ -17,11 +21,18 @@ function drawBoundary() {
 }
 
 function setup() {
-  createCanvas(1200, 800);
-  grid = new Grid(240, 160, 0.3, true);
+  createCanvas(1700, 900);
+  grid = new Grid(int(width / 10), int(height / 10), 0.3, diagonals);
   scaleX = width / grid.Nx;
   scaleY = height / grid.Ny;
-  scaleP = int(Math.sqrt(grid.Nx + grid.Ny));
+  scaleP = int(
+    Math.sqrt(1 / (1 / grid.Nx + 1 / grid.Ny)) *
+      (1 + (actualDistance !== distance))
+  );
+  // scaleP = int(
+  //   Math.sqrt(1 / (1 / grid.Nx + 1 / grid.Ny)) *
+  //     (1 + (actualDistance !== distance) - 0.5 * (actualDistance === distance))
+  // );
 
   aStar = new AStar(
     grid.cells[0][0],
@@ -36,14 +47,15 @@ function setup() {
 }
 
 function draw() {
-  count++;
-  if (aStar.openSet.head) aStar.iterate();
-  else console.log("NO PATH FOUND!");
+  // count++;
+  for (let n = 0; n < speed; n++)
+    if (aStar.openSet.head) aStar.iterate();
+    else console.log("NO PATH FOUND!");
   // aStar.plotOpen(scaleX, scaleY);
 
   // if (!(count % 20)) {
-    // aStar.plotClosed(scaleX, scaleY);
-    // aStar.plotOpen(scaleX, scaleY);
+  // aStar.plotClosed(scaleX, scaleY);
+  // aStar.plotOpen(scaleX, scaleY);
   // }
   // if (aStar.closedUpdated) aStar.plotClosed(scaleX, scaleY);
   // if (aStar.openUpdated) aStar.plotOpen(scaleX, scaleY);
